d482d81acfcff0e5fd34e7bec7931bc550158277
BdonGordon <bgordon@linamar.com>
2018-12-20 16:05:28 -0500

    - Added mirage and then the ember-bootstrap

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   beforeModel() {
+    //what we're doing here is replacing the current URL in the browser's history. transitionTo() just adds to the history
     this.replaceWith('rentals');
   }
 });
--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -1,4 +1,38 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  /*This function acts as a hook. Ember will call it for us during different times in the app.
+  * Ember will use this model object and save it as an attribute called model
+  */
+  model() {
+    return [
+      {
+        id: 1,
+        title: 'Genesis Coupe',
+        model: 2018,
+        company: 'Hyundai',
+        horsePower: 220,
+        price: 23000,
+        image: '../../public/genesis.jpg'
+      },
+      {
+        id: 2,
+        title: 'Lamborghini Merci',
+        model: 2018,
+        company: 'Lamborghini',
+        horsePower: 320,
+        price: 400000,
+        image: '../../public/lambo.jpg'
+      },
+      {
+        id: 3,
+        title: 'Porsche 911',
+        model: 2017,
+        company: 'Porsche',
+        horsePower: 340,
+        price: 530000,
+        image: '../../public/porsche.jpg'
+      }
+    ];
+  }
 });
