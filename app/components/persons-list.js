b7be6ddf939d0ab547ed6011d8e34d1ca7e806d5
BdonGordon <bgordon@linamar.com>
2018-12-20 10:19:37 -0500

    - Restarting tutorial from scratch

--- app/components/persons-list.js
+++
@@ -1,6 +0,0 @@
-import Component from '@ember/component';
-
-/** This file, as well as templates/components/persons-list.hbs, was auto-generated via cmd line: ember generate component persons-list*/
-/** A component is a UI element that will be utilized between multiple pages (just like a component in react) */
-export default Component.extend({
-});
