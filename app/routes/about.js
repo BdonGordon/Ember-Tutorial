0bea82510291726e5786d829e1ed0310cddcca51
BdonGordon <bgordon@linamar.com>
2018-12-20 13:43:23 -0500

    - Added contact and about routes --> Added content to them

---
+++ app/routes/about.js
@@ -0,0 +1,5 @@
+import Route from '@ember/routing/route';
+
+/** When the route is loaded, this is where we make everything that should be on the page's content **/
+export default Route.extend({
+});
